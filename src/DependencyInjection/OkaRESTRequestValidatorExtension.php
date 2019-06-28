d49876adbc00caadc17cc46079c9d213adb4e97a
Cedrick Oka Baidai <cedric.baidai@veone.net>
2019-06-28 16:01:30 +0000

    * Upgrade configuration node definition.

--- src/DependencyInjection/OkaRESTRequestValidatorExtension.php
+++ src/DependencyInjection/OkaRESTRequestValidatorExtension.php
@@ -7,9 +7,9 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\DependencyInjection\Parameter;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
  * This is the class that loads and manages your bundle configuration.
@@ -32,21 +32,13 @@ class OkaRESTRequestValidatorExtension extends Extension
 		$definition = $container->getDefinition(ErrorResponseFactory::class);
 		$definition->replaceArgument(0, $config['response']['error_builder_class']);
 		
-		if (true === $config['enabled_exception_listener']) {
-// 			public function __construct($controller, LoggerInterface $logger = null, $debug = false)
+		if (true === $config['exception']['enabled']) {
 			$exceptionListener = $container->setDefinition('oka_rest_request_validator.exception.event_listener', new Definition(ExceptionListener::class));
-			$exceptionListener->addArgument($config['exception_controller']);
+			$exceptionListener->addArgument($config['exception']['controller']);
 			$exceptionListener->addArgument(new Reference('logger'));
 			$exceptionListener->addArgument(new Parameter('kernel.debug'));
 			$exceptionListener->addTag('kernel.event_subscriber');
 			$exceptionListener->addTag('monolog.logger', ['channel' => 'request']);
-			
-// 			$exceptionListener->addArgument(new Reference('translator'));
-// 			$exceptionListener->addArgument(new Reference('oka_rest_request_validator.error_response.factory'));
-// 			$exceptionListener->addArgument(new Reference('logger'));
-// 			$exceptionListener->addArgument(new Parameter('kernel.environment'));
-// 			$exceptionListener->addTag('kernel.event_listener', ['event' => 'kernel.exception', 'method' => 'onKernelException']);
-// 			$exceptionListener->addTag('monolog.logger', ['channel' => 'request']);
 		}
 	}
 }
